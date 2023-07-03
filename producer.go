b5b80d2122cfdeca5e6f9f597daadb71cc8a1822
张俊超 <zhangjunchao@xiaohulu.com>
2023-07-03 14:02:38 +0800

    1.新增延迟消息和异步消息

--- producer.go
+++ producer.go
@@ -3,6 +3,7 @@ package mbs_queue
 import (
 	"context"
 	rmqClient "github.com/apache/rocketmq-clients/golang/v5"
+	"time"
 )
 
 type ProducerOptions struct {
@@ -52,6 +53,7 @@ func WithConsoleEnabled(enabled bool) ProducerOption {
 	}
 }
 
+// SendNormalMessage 普通同步无序消息
 func (p *Producer) SendNormalMessage(ctx context.Context, topic string, tag string, messageBody string) (string, error) {
 	var messageID string
 
@@ -60,6 +62,86 @@ func (p *Producer) SendNormalMessage(ctx context.Context, topic string, tag stri
 		Body:  []byte(messageBody),
 		Tag:   &tag,
 	}
+
+	serRes, err := p.Producer.Send(ctx, &msg)
+	if err != nil {
+		return messageID, err
+	}
+
+	for _, re := range serRes {
+		tmp := re
+		messageID = tmp.MessageID
+	}
+
+	return messageID, nil
+}
+
+// SendAsyncMessage 普通异步无序消息
+func (p *Producer) SendAsyncMessage(ctx context.Context, topic string, tag string, messageBody string) (string, error) {
+	var (
+		messageID string
+		errs      error
+	)
+
+	msg := rmqClient.Message{
+		Topic: topic,
+		Body:  []byte(messageBody),
+		Tag:   &tag,
+	}
+
+	p.Producer.SendAsync(ctx, &msg, func(ctx context.Context, resp []*rmqClient.SendReceipt, err error) {
+		if err != nil {
+			errs = err
+			return
+		}
+
+		for _, re := range resp {
+			tmp := re
+			messageID = tmp.MessageID
+		}
+
+		return
+	})
+
+	return messageID, errs
+}
+
+// SendFifoMessage 同步有序消息
+func (p *Producer) SendFifoMessage(ctx context.Context, topic string, tag string, messageBody string) (string, error) {
+	var messageID string
+
+	msg := rmqClient.Message{
+		Topic: topic,
+		Body:  []byte(messageBody),
+		Tag:   &tag,
+	}
+
+	msg.SetMessageGroup("fifo")
+
+	serRes, err := p.Producer.Send(ctx, &msg)
+	if err != nil {
+		return messageID, err
+	}
+
+	for _, re := range serRes {
+		tmp := re
+		messageID = tmp.MessageID
+	}
+
+	return messageID, nil
+}
+
+// SendDelayMessage 同步无序延时消息
+func (p *Producer) SendDelayMessage(ctx context.Context, topic string, tag string, messageBody string, triggerTime time.Time) (string, error) {
+	var messageID string
+
+	msg := rmqClient.Message{
+		Topic: topic,
+		Body:  []byte(messageBody),
+		Tag:   &tag,
+	}
+
+	msg.SetDelayTimestamp(triggerTime)
 
 	serRes, err := p.Producer.Send(ctx, &msg)
 	if err != nil {
